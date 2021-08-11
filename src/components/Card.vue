ac52d2d85889e8859622f138ec7fa0fc882a87f7
Daniel <dtheran1.0@gmail.com>
2021-08-11 11:11:05 -0500

    carrito de compras

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="col-12 col-sm-4 col-md-3 col-lg-2 mb-3">
+    <div class="card">
+      <img
+        :src="producto.thumbnailUrl"
+        :alt="producto.title"
+        class="card-img-top"
+      />
+      <div class="card-body">
+        <h5>{{ producto.title }}</h5>
+        <p>{{ producto.precio }}</p>
+        <button class="btn btn-dark" @click="agregar(producto)">Comprar</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { useStore } from "vuex";
+
+export default {
+  props: ["producto"],
+  setup() {
+    const store = useStore();
+    const agregar = (producto) => {
+        store.dispatch('agregarAlCarro', producto)
+}
+    return {
+        agregar
+    }
+  },
+};
+</script>
